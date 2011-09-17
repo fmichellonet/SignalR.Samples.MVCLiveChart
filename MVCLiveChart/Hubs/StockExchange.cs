5af32eebd102424893e22d24b4eae2d8fd5ab87c
Fabrice Michellonet <fabrice.michellonet@gmail.com>
2011-09-18 00:18:34 +0200

    Initial commit

---
+++ MVCLiveChart/Hubs/StockExchange.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+using SignalR.Hubs;
+
+namespace MVCLiveChart.Hubs
+{
+    public class StockExchange : Hub
+    {
+        private Timer _timer;
+
+        private double _msftPrice = 10.2;
+
+        private double VaryPrice()
+        {
+            Random rnd = new Random();
+            _msftPrice = _msftPrice + (rnd.Next(-1, 2) * rnd.NextDouble());
+            return _msftPrice;
+        }
+
+        public bool Connect()
+        {
+            // Set unique id for client.
+            Caller.Id = Context.ClientId;
+
+            _timer = new Timer { Interval = 1000 };
+
+            _timer.Elapsed += (sender, e) => Send(
+                new List<SharePrice>(new[]
+                                         {
+                                             new SharePrice
+                                                 {
+                                                     Date = DateTime.Now,
+                                                     Price = VaryPrice(),
+                                                     Share = new Share {Name = "Fowler-Corp"}
+                                                 }
+                                         }));
+            _timer.Start();
+
+            return true;
+        }
+
+        public void Send(IEnumerable<SharePrice> sp)
+        {
+            Clients.sendSharePrices(Context.ClientId, sp);
+        }
+    }
+
+    public class Share
+    {
+        public string Name { get; set; }
+    }
+
+    public class SharePrice
+    {
+        public Share Share { get; set; }
+        public DateTime Date { get; set; }
+        public double Price { get; set; }
+    }
+
+
+}
---
+++ MVCLiveChart/Scripts/StockExchangeClient.js
@@ -0,0 +1,99 @@
+﻿/// <reference path="../../Scripts/jquery-1.6.2.js" />
+/// <reference path="../../Scripts/jQuery.tmpl.js" />
+/// <reference path="../../Scripts/jquery.cookie.js" />
+
+
+$(function () {
+
+    window.Highcharts.setOptions({
+        global: {
+            useUTC: false
+        }
+    });
+
+    var chart;
+
+    $(document).ready(function () {
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'container',
+                defaultSeriesType: 'spline',
+                marginRight: 10
+            },
+            title: {
+                text: 'Real time charting with SignalR'
+            },
+            xAxis: {
+                type: 'datetime',
+                tickPixelInterval: 150
+            },
+            yAxis: {
+                title: {
+                    text: 'Value'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.series.name + '</b><br/>' +
+							Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+							Highcharts.numberFormat(this.y, 2);
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            exporting: {
+                enabled: false
+            },
+            series: [{
+                name: 'Fowler-Corp',
+                data: (function () {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
+                    for (i = -19; i <= 0; i++) {
+                        data.push({
+                            x: time + i * 1000,
+                            y: Math.random()
+                        });
+                    }
+                    return data;
+                })()
+            }]
+        });
+
+    });
+
+    var stockExchangeServer = $.connection.stockExchange;
+
+    stockExchangeServer.sendSharePrices = function (id, content) {
+        //console.log(content);
+
+        if (id != this.Id) {
+            return;
+        }
+
+        // set up the updating of the chart.
+        var series = chart.series[0];
+        var x = new Date(parseInt(content[0].Date.substr(6))).getTime(),
+                y = content[0].Price;
+
+        series.addPoint([x, y], true, true);
+    };
+
+    $.connection.hub.start(function () {
+        stockExchangeServer.connect()
+            .done(function (success) {
+                if (success === false) {
+                    console.log(":(");
+                }
+                console.log("connected");
+            });
+    });
+});
